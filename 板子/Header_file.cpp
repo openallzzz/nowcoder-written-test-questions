9c50e4e525ca2478a542b2eb4180c2c240dd64b3
zhangyu <1802833394@qq.com>
2023-01-26 00:30:20 +0800

    添加一个文件夹存放模板

---
+++ 板子/Header_file.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+#define x first
+#define y second
+
+// @author: openallzzz
+using namespace std;
+
+typedef long long LL;
+typedef pair<LL, LL> PLL;
+typedef pair<int, int> PII;
+
+const int N = 1e5 + 10;
+
+int main() {
+    
+}
